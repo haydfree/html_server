24e0fe553b3848dc0e4ef75ea7dd802be20f9b01
Hayden Freeman <haydenfreeman@yahoo.com>
2025-01-03 11:59:48 -0600

    ready for api functionality

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
 #define PORT 8080
 #define ERROR -1
@@ -19,23 +20,44 @@ enum AddressFamily { IPV4 = AF_INET, IPV6 = AF_INET6 };
 enum SocketType { TCP = SOCK_STREAM, UDP = SOCK_DGRAM };
 enum SocketProtocol {TCP_PROTOCOL = IPPROTO_TCP, UDP_PROTOCOL = IPPROTO_UDP };
 enum AddressOption { ANY = INADDR_ANY, LOOPBACK = INADDR_LOOPBACK };
-enum ErrorType {SVR_ADDR_INIT, SVR_SOCK_INIT, CLI_ADDR_INIT, CLI_SOCK_INIT, BIND, LISTEN, RECV };
-const char *ErrorTypeStr[7] = {"SVR_ADDR_INIT", "SVR_SOCK_INIT", "CLI_ADDR_INIT", 
-                            "CLI_SOCK_INIT", "BIND", "LISTEN", "RECV" };
 
-void checkForErrors(int data, enum ErrorType type) {
-    if (data != -1) { return; }
-    printf("Error at: %s \n", ErrorTypeStr[type]);
+typedef struct HttpHeader {
+    char *name;
+    char *value;
+} HttpHeader;
+
+typedef struct HttpRequest {
+    char *method;
+    char *path;
+    HttpHeader *headers;
+    char *body;
+} HttpRequest;
+
+typedef struct HttpResponse {
+    int statusCode;
+    HttpHeader *headers;
+    char *body;
+} HttpResponse;
+
+void checkForErrors(int code, int *data, char *file, const char *func, int line, char *msg) {
+    if (*data != -1) { return; }
+    fprintf(stderr, 
+            "Error: %s\n"
+            "Code: %d\n" 
+            "Location: %s:%d in %s\n",
+            msg, code, file, line, func
+            );
+    exit(code);
 }
 
-void checkForNull(void *data, enum ErrorType type) {
+void checkForNull(int code, int *data, char *file, const char *func, int line, char *msg) {
     if (data != NULL) { return; }
-    checkForErrors(-1, type);
+    checkForErrors(code, data, file, func, line, msg);
 }
 
 void initServerAddress(int serverPort) {
     p_serverAddress = malloc(sizeof(Address));
-    checkForNull(p_serverAddress, SVR_ADDR_INIT);
+    checkForNull(-1, p_serverAddress, __FILE__, __func__, __LINE__, "p_serverAddress is null");
     memset(p_serverAddress, 0, sizeof(Address));
 
     serverPort = htons(serverPort);
@@ -44,26 +66,49 @@ void initServerAddress(int serverPort) {
     p_serverAddress->sin_port = serverPort;
 }
 
+HttpRequest *buildRequest(char *method, char *path, HttpHeader *headers, char *body) {
+    HttpRequest *req = malloc(sizeof(HttpRequest));
+    checkForNull(-1, req, __FILE__, __func__, __LINE__, "request is null");
+
+    req->method = method;
+    req->path = path;
+    req->headers = headers;
+    req->body = body;
+
+    return req;
+}
+void parseRequest();
+void sendRequest();
+
+void buildResponse();
+void parseResponse();
+void receiveResponse();
+
 int main() {
     serverSocket = socket(IPV4, TCP, TCP_PROTOCOL); 
-    checkForErrors(serverSocket, SVR_SOCK_INIT);
-    
+    checkForErrors(-1, &serverSocket, __FILE__, __func__, __LINE__, "serverSocket");
+
     initServerAddress(PORT);
 
     int bindRes = bind(serverSocket, (struct sockaddr *) p_serverAddress, sizeof(Address));
-    checkForErrors(bindRes, BIND);
+    checkForErrors(-1, &bindRes, __FILE__, __func__, __LINE__, "bindres");
 
     int listenRes = listen(serverSocket, BACKLOG_SIZE);
-    checkForErrors(listenRes, LISTEN);
+    checkForErrors(-1, &listenRes, __FILE__, __func__, __LINE__, "listenRes");
 
     while (1) {
         int clientSocket = accept(serverSocket, NULL, NULL);
-        checkForErrors(clientSocket, CLI_SOCK_INIT);
+        checkForErrors(-1, &clientSocket, __FILE__, __func__, __LINE__, "clientSocket");
+
 
         char buffer[BUFFER_SIZE];
+        memset(buffer, 0, BUFFER_SIZE);
         int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-        checkForErrors(bytesReceived, RECV);
+        checkForErrors(-1, &bytesReceived, __FILE__, __func__, __LINE__, "bytesReceived");
+
         buffer[bytesReceived] = '\0';
+
+        printf("request: %s\n", buffer);
 
         const char *httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, world!";
         send(clientSocket, httpResponse, strlen(httpResponse), 0);
